928085aee8ba851e44a2b3e06083a07e5e41f66d
elan33 <67664518@qq.com>
2019-04-04 10:45:50 +0800

    cerberusos bringup

--- cerberus_nx569j.mk
+++ cerberus_nx569j.mk
@@ -18,20 +18,24 @@
 TARGET_SCREEN_HEIGHT := 1080
 TARGET_SCREEN_WIDTH := 1920
 
+CERBERUS_BUILD_TYPE := OFFICIAL
+
+CERBERUS_MAINTAINER := jsbeyond
+
 # Inherit 64-bit configs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common CerberusOS stuff.
+$(call inherit-product, vendor/cerberus/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/nubia/nx569j/device.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_nx569j
+PRODUCT_NAME := cerberus_nx569j
 PRODUCT_DEVICE := nx569j
 PRODUCT_BRAND := nubia
 PRODUCT_MODEL := NX569J
